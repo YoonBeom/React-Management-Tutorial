7dbee429ae9977f9967413cf1869121a90ce86ea
Yoonbeom Park <overcam1@gmail.com>
2020-04-25 20:28:45 +0900

    Add Get Customers REST API Module

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/customers', (req, res) => {
+  res.send([
+    {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/any',
+      'name': '홍길동',
+      'birthday': '920501',
+      'gender': '남자',
+      'job': '대학생'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '이번이',
+      'birthday': '920501',
+      'gender': '여자',
+      'job': '중학생'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '삼번이',
+      'birthday': '920501',
+      'gender': '남자',
+      'job': '대학생'
+    }
+  ]);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
